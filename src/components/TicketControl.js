e7898203d0b15fd0f9440bc4282b011a0befcbac
Benjamin Thom <jaminthom@gmail.com>
2020-05-04 11:07:56 -0700

    remove instances of ADD_TICKET action from project
    Co-authored-by: jeremy kale padot <jeremy@padot.us>
    Co-authored-by: Brandan Sayarath <brandan@brandan.tech>
    Co-authored-by: Joseph <45412175+fractalscape13@users.noreply.github.com>
    Co-authored-by: Patrick Delaney <prestwick97@gmail.com>

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -47,15 +47,15 @@ class TicketControl extends React.Component {
     this.setState({editing: true});
   }
 
-  handleEditingTicketInList = (ticketToEdit) => {
-    const { dispatch } = this.props;
-    const action = a.addTicket(ticketToEdit);
-    dispatch(action);
-    this.setState({
-      editing: false,
-      selectedTicket: null
-    });
-  }
+  // handleEditingTicketInList = (ticketToEdit) => {
+  //   const { dispatch } = this.props;
+  //   const action = a.addTicket(ticketToEdit);
+  //   dispatch(action);
+  //   this.setState({
+  //     editing: false,
+  //     selectedTicket: null
+  //   });
+  // }
 
   render(){
     let currentlyVisibleState = null;
